ba4c11555eb7640d388f8a71b2fdbb13b8322ed8
Bektasiadis Efstratios <ebektasiadis@gmail.com>
2024-03-22 17:25:01 +0200

    feat: added initial movie catalogue

--- src/app/movies/movie-catalogue/movie-catalogue.component.ts
+++ src/app/movies/movie-catalogue/movie-catalogue.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Movie } from '../movie.model';
+import { MoviesService } from '../movies.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-movie-catalogue',
   templateUrl: './movie-catalogue.component.html',
 })
-export class MovieCatalogueComponent {}
+export class MovieCatalogueComponent implements OnInit, OnDestroy {
+  private getPopularSubscription$: Subscription;
+
+  movies: Movie[] = [];
+
+  constructor(private moviesService: MoviesService) {
+    this.getPopularSubscription$ = Subscription.EMPTY;
+  }
+
+  ngOnInit() {
+    this.getPopularSubscription$ = this.moviesService
+      .getPopular()
+      .subscribe((response) => {
+        this.movies = response.results;
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.getPopularSubscription$) {
+      this.getPopularSubscription$.unsubscribe();
+    }
+  }
+}
