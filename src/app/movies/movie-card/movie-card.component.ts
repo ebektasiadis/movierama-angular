e5b18b23414a5a718c715774c1c81dae7669757c
Bektasiadis Efstratios <ebektasiadis@gmail.com>
2024-03-22 17:24:36 +0200

    feat: added initial movie card

--- src/app/movies/movie-card/movie-card.component.ts
+++ src/app/movies/movie-card/movie-card.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../movie.model';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
 })
-export class MovieCardComponent {}
+export class MovieCardComponent {
+  @Input() movie!: Movie;
+
+  get imageUrl() {
+    return `https://image.tmdb.org/t/p/w300${this.movie.poster_path}`;
+  }
+
+  get imageAlt() {
+    return `${this.movie.title} movie poster`;
+  }
+}
